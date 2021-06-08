b6147ed8fd370bcf60ea5f3bb52702982e5e10fb
Sudha Parimala <sudharg247@gmail.com>
2021-06-08 12:54:56 +0530

    add repo

--- .ci.sh
+++ .ci.sh
@@ -2,6 +2,7 @@
 opam update -u
 opam switch create ${OCAML_VERSION} --packages=ocaml-variants.${OCAML_VERSION} --repositories=multicore=git+https://github.com/ocaml-multicore/multicore-opam.git,default
 eval $(opam env)
+opam repository add multicore https://github.com/ocaml-multicore/multicore-opam.git
 opam install dune domainslib
 
 cd code
